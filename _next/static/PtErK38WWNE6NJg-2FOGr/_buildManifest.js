882f14a790d9041bc36b38a96b8cf722bc41e655
Yunho Lee <yunho@example.com>
2026-03-10 08:40:12 +0000

    Force deploy roulette feature

--- _next/static/PtErK38WWNE6NJg-2FOGr/_buildManifest.js
+++ _next/static/PtErK38WWNE6NJg-2FOGr/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/pixel-vault//_next/:path+",
-        "destination": "/pixel-vault/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
